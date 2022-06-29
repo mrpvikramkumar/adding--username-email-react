af035a18cac9285693acd37ed58ca528c3cdf999
Vikram Kumar <81801195+mrpvikramkumar@users.noreply.github.com>
2022-06-30 01:31:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import BasicForm from './components/BasicForm';
+import Footer from './Footer/Footer'
+function App() {
+  return (
+    <div>
+    <div className="app">
+      <BasicForm />
+    </div>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
